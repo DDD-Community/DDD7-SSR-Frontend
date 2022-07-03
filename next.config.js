e92a65f3cddd57f74d33d7dbb7b43857083126f9
geonwoomun <ansejrrhkd@naver.com>
2022-07-03 16:31:23 +0900

    feat: dewspaper image 허용

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['upload2.inven.co.kr', 'lh3.googleusercontent.com'],
+    domains: ['upload2.inven.co.kr', 'lh3.googleusercontent.com', 'ddd-project.s3.ap-northeast-2.amazonaws.com'],
   },
 };
 
