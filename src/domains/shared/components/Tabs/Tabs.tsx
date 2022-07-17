a3e6e818b2f8901645c1063513b124174b83d2c5
geonwoomun <ansejrrhkd@naver.com>
2022-07-17 18:47:33 +0900

    style: tabs 스타일 수정

--- src/domains/shared/components/Tabs/Tabs.tsx
+++ src/domains/shared/components/Tabs/Tabs.tsx
@@ -1,11 +1,11 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { useState, useCallback, useRef } from 'react';
 import { Color } from '../../constants';
-import { useBreakPointStore } from '../../store/breakPoint';
 import { Text } from '../Text';
 import { TabsProps } from './TabsType';
 
-function Tabs({ tabList, onTabChange }: TabsProps) {
+function Tabs({ tabList, onTabChange, useUpperLine = true, tabGap, useInlineTab }: TabsProps) {
   const ref = useRef<HTMLDivElement>(null);
 
   const [tabView, setTabView] = useState(tabList[0].value);
@@ -22,47 +22,71 @@ function Tabs({ tabList, onTabChange }: TabsProps) {
   );
 
   return (
-    <TabsList ref={ref}>
+    <TabsList ref={ref} useUpperLine={useUpperLine} useInlineTab={useInlineTab}>
       {tabList.map((tab) => (
-        <Tab className={tabView === tab.value ? 'active' : ''} key={tab.value} onClick={onClickTab(tab.value)}>
-          <Text type="body16" color="White100">
-            {tab.label}
-          </Text>
+        <Tab
+          className={tabView === tab.value ? 'active' : ''}
+          useUpperLine={useUpperLine}
+          key={tab.value}
+          onClick={onClickTab(tab.value)}
+          tabGap={tabGap}
+        >
+          {tab.label}
         </Tab>
       ))}
     </TabsList>
   );
 }
 
-const TabsList = styled.nav`
-  display: flex;
-  justify-content: space-around;
-  position: relative;
-  min-width: 300px;
-  height: 70px;
-  border-top: 1px solid ${Color.Gray700};
-`;
+const TabsList = styled.nav(
+  ({ useUpperLine, useInlineTab }: Pick<TabsProps, 'useUpperLine' | 'useInlineTab'>) => css`
+    position: relative;
+    min-width: 300px;
+    height: 35px;
+    ${!useInlineTab &&
+    css`
+      display: flex;
+      justify-content: space-around;
+    `}
+    ${useUpperLine &&
+    css`
+      border-top: 1px solid ${Color.Gray700};
+    `}
+  `,
+);
 
-const Tab = styled.div`
-  display: inline-flex;
-  flex: 1;
-  justify-content: center;
-  position: relative;
-  cursor: pointer;
-  padding-top: 16px;
+const Tab = styled.div(
+  ({ useUpperLine, tabGap }: Pick<TabsProps, 'useUpperLine' | 'tabGap'>) => css`
+    display: inline-flex;
+    flex: 1;
+    justify-content: center;
+    position: relative;
+    cursor: pointer;
+    padding-top: 16px;
 
-  &::before {
-    content: '';
-    position: absolute;
-    top: -1px;
-    width: 100%;
-    border-top: 1px solid transparent;
-    transition: all 0.3s linear;
-  }
+    ${tabGap &&
+    css`
+      &:not(:last-of-type) {
+        margin-right: ${tabGap}px;
+      }
+    `}
 
-  &.active::before {
-    border-top: 1px solid ${Color.Primary100};
-  }
-`;
+    ${useUpperLine &&
+    css`
+      &::before {
+        content: '';
+        position: absolute;
+        top: -1px;
+        width: 100%;
+        border-top: 1px solid transparent;
+        transition: all 0.3s linear;
+      }
+
+      &.active::before {
+        border-top: 1px solid ${Color.Primary100};
+      }
+    `}
+  `,
+);
 
 export default Tabs;
--- src/domains/shared/components/Tabs/TabsType.ts
+++ src/domains/shared/components/Tabs/TabsType.ts
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TextType } from '../Text/Text';
 
 interface Tab {
   value: string;
@@ -6,5 +7,8 @@ interface Tab {
 }
 export interface TabsProps {
   tabList: Tab[];
+  tabGap?: number;
+  useInlineTab?: boolean;
   onTabChange: (value: string) => void;
+  useUpperLine?: boolean;
 }
