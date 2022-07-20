895fd10a5c53b1f8a9a0cd308de3adb31f30387b
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-07-20 23:00:06 +0900

    style: 윈도우 폰트 깨짐 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function MyDocument() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
       </Head>
       <body>
         <Main />
