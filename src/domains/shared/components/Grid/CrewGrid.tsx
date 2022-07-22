e11a5954e82419b57aac78f81b88bbbd9fc8a867
geonwoomun <ansejrrhkd@naver.com>
2022-07-22 22:24:51 +0900

    fix: Grid 관련 수정

--- src/domains/shared/components/Grid/CrewGrid.tsx
+++ src/domains/shared/components/Grid/CrewGrid.tsx
@@ -1,7 +1,9 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 import { BreakPoint } from '../../hooks/useMediaQuery';
 import { CrewCard, CrewCardProps } from '../CrewCard';
+import { GridProps } from './GridType';
 
 export interface CrewGridProps {
   contents: CrewCardProps[];
@@ -12,7 +14,7 @@ export const CrewGrid = ({ contents, loadMore }: CrewGridProps) => {
   const { containerRef } = useInfiniteScroll({ dataLength: contents?.length, loadMore });
 
   return (
-    <GridWrapperStyle ref={containerRef}>
+    <GridWrapperStyle ref={containerRef} isFullWidth={contents.length > 2}>
       {contents.map((crew) => (
         <CrewCard key={crew.accountIdx} {...crew} />
       ))}
@@ -20,14 +22,17 @@ export const CrewGrid = ({ contents, loadMore }: CrewGridProps) => {
   );
 };
 
-const GridWrapperStyle = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 296px);
-  gap: 21px;
-  max-width: 1256px;
-  width: 100%;
+const GridWrapperStyle = styled.div(
+  ({ isFullWidth }: GridProps) => css`
+    display: grid;
+    justify-content: ${isFullWidth ? 'center' : 'flex-start'};
+    grid-template-columns: repeat(auto-fit, 296px);
+    gap: 21px;
+    max-width: 1256px;
+    width: 100%;
 
-  ${BreakPoint.Mobile()} {
-    justify-content: center;
-  }
-`;
+    ${BreakPoint.Mobile()} {
+      justify-content: center;
+    }
+  `,
+);
--- src/domains/shared/components/Grid/PostGrid.tsx
+++ src/domains/shared/components/Grid/PostGrid.tsx
@@ -1,8 +1,10 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 import { BreakPoint } from '../../hooks/useMediaQuery';
 import { PostDetail } from '../../model/post';
 import { PostCard } from '../PostCard';
+import { GridProps } from './GridType';
 
 interface PostGridProps {
   contents: PostDetail[];
@@ -13,7 +15,7 @@ export const PostGrid = ({ contents, loadMore }: PostGridProps) => {
   const { containerRef } = useInfiniteScroll({ dataLength: contents?.length, loadMore });
 
   return (
-    <PostWrapperStyle ref={containerRef}>
+    <PostWrapperStyle ref={containerRef} isFullWidth={contents.length > 2}>
       {contents.map((post) => (
         <PostCard key={post.postIdx} data={post} />
       ))}
@@ -21,13 +23,17 @@ export const PostGrid = ({ contents, loadMore }: PostGridProps) => {
   );
 };
 
-const PostWrapperStyle = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 296px);
-  gap: 21px;
-  width: 100%;
+const PostWrapperStyle = styled.div(
+  ({ isFullWidth }: GridProps) => css`
+    display: grid;
+    justify-content: ${isFullWidth ? 'center' : 'flex-start'};
+    grid-template-columns: repeat(auto-fit, 296px);
+    gap: 21px;
+    width: 100%;
+    max-width: 1256px;
 
-  ${BreakPoint.Mobile()} {
-    justify-content: center;
-  }
-`;
+    ${BreakPoint.Mobile()} {
+      justify-content: center;
+    }
+  `,
+);
